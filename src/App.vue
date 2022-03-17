37231393e43fa1e80ccac8a2424ad92ba6d55b02
agonzalo19 <74074576+agonzalo19@users.noreply.github.com>
2022-03-17 18:53:03 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+<caption>Iron Contacts</caption>
+<td>
+<th>Pictures</th>
+<th>Name</th>
+<th>popularity</th>
+</td>
+  <table v-for="contact in myContacts.slice(0, 5)" :key="contact"> 
+    <td> <img :src="contact.pictureUrl" alt=""></td>
+    <td>{{ contact.name }}</td>
+    <td>{{ contact.popularity }}</td>
+   </table>
+</template>
+
+<script setup>
+
+import contacts from "./contacts.json";
+const myContacts = contacts 
+// (() => {
+//   return contacts.lenght < 3
+//     })
+//console.log(myContacts.length);
+
+
+</script>
+
+<style>
+th{
+  padding: 20px;
+}
+
+img{
+  height: 100px;
+  width: 100%;
+}
+
+td{
+  padding: 20px;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
